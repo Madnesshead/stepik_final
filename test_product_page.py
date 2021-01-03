308cc808d11a8cef0209384d134ea3493feb2ed6
Natallia Akulich <vladimirrovna@gmail.com>
2021-01-03 19:57:29 +0300

    Added test to check empty basket from product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
+import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
-import pytest
+from .pages.basket_page import BasketPage
+
 
 link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
 
@@ -50,3 +52,10 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket = BasketPage(browser, browser.current_url)
+    basket.should_not_have_items()
+    basket.should_have_message_if_empty()
