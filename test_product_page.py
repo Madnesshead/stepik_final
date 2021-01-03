54f5ab0d0cc067fcb7dd7554bce8330bf5121391
Natallia Akulich <vladimirrovna@gmail.com>
2021-01-03 17:04:24 +0300

    Moved functions from main page to base page, updated locators for base page, added tests for going from product page to login page

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,17 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
-import time
 
 link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
 
-# @pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
-#                                          "8", "9"])
-# def test_guest_can_add_product_to_basket(browser, promo_offer):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-#     product_page = ProductPage(browser, link)
-#     product_page.open()
-#     product_page.should_be_product_page()
+
+@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
+                                         "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_product_page()
 
 
 @pytest.mark.xfail(reason="failed for checking that selector for success message is correct")
@@ -34,4 +35,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.add_product_to_basket()
     product_page.should_disappear_success_message()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
